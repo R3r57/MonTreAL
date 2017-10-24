106bde3da9ecef4d048bb3a14798b261511e5fe9
Cornelius Matějka <matejka.cornelius@mailbox.org>
2017-10-24 10:32:28 +0200

    MetaDataAppender configuration now in JSON fromat for more flexibility

--- src/lib/utilities/metadata_appender.py
+++ src/lib/utilities/metadata_appender.py
@@ -10,20 +10,20 @@ class MetaDataAppender(threading.Thread):
         super(MetaDataAppender, self).__init__()
         self.name = name
         self.event = event
+        self.config = config
         self.input_queue = input_queue
         self.output_queue = output_queue
         self.hostname = None
         self.machine_id = None
         self.building = None
         self.room = None
-        self.__init_metadata(config)
+        self.__init_metadata()
         logger.info("{} initialized successfully".format(self.name))
 
     def run(self):
         logger.info("Started {}".format(self.name))
         while not self.event.is_set():
             self.event.wait(2)
-
             while not self.input_queue.empty():
                 raw = self.input_queue.get()
                 logger.info("Raw data received")
@@ -35,15 +35,30 @@ class MetaDataAppender(threading.Thread):
                 logger.info("Data put in queue")
         logger.info("Stopped: {}".format(self.name))
 
-    def __init_metadata(self, config):
-        tmp = self.__get_file_content(config["hostname_path"])
-        self.hostname = tmp[0].split("\n")[0]
-        tmp2 = self.__get_file_content(config["machine_id_path"])
-        self.machine_id = tmp2[0].split("\n")[0]
-        location_info = self.__get_file_content(config["location_info_path"])
-        self.building = location_info[0].split("=")[1].split("\n")[0]
-        self.room = location_info[1].split("=")[1].split("\n")[0]
-        logger.info("Configuration: {}, {}, {}, {}".format(self.hostname, self.machine_id, self.building, self.room))
+    def __init_metadata(self):
+        local_configuration = self.__get_local_configuration()
+        self.hostname = self.__get_hostname(local_configuration['hostname'])
+        self.machine_id = local_configuration['machine_id']
+        self.building = local_configuration['building']
+        self.room = local_configuration['room']
+
+    def __get_hostname(self, path):
+        if os.path.isfile(path):
+            with open(path) as file:
+                return file.readline().strip()
+        else:
+            logger.error("Unable to locate {} for hostname".format(path))
+            return "unspecified"
+
+    def __get_local_configuration(self):
+        local_configuration_path = self.config['local_configuration']
+        logger.info("Local configuration file set to {}".format(local_configuration_path))
+        if os.path.isfile(local_configuration_path):
+            with open(local_configuration_path, 'r') as file:
+                data = json.load(file)
+                return data
+        else:
+            logger.error("Local configuration file not found: {}".format(local_configuration_path))
 
     def __get_file_content(self, filepath):
         if os.path.isfile(filepath):
